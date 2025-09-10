7b7810f3c997681a051bd5e8a756693d1da73210
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-09-10 22:42:24 +0200

    feat: splitter liste i navn og alder med annenhver indeks (oppgave 2.2)

---
+++ src/oppgave_2_2.py
@@ -0,0 +1,16 @@
+def list_splitting():
+    
+        personer = ["Cecilie", 28, "Bjørn", 30,"Tor", 24,"Anna", 25]
+        # Split the list into two parts
+        navn = []
+        alder = []
+        for i in range(0,len(personer), 2):
+                navn.append(personer[i])
+                alder.append(personer[i + 1])
+        print("Navn-liste:", navn)
+        print("Alder-liste:", alder)
+
+
+#Programstart
+if __name__ == "__main__":
+    list_splitting()
