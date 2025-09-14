af29b7916ef6bc43a9c6228085185a774996caf0
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-09-14 15:34:44 +0200

    Testning. klar kode

--- src/oppgave_2_2.py
+++ src/oppgave_2_2.py
@@ -1,10 +1,10 @@
 def list_splitting():
     
         personer = ["Cecilie", 28, "Bjørn", 30,"Tor", 24,"Anna", 25]
-        # Split the list into two parts
+
         navn = []
         alder = []
-        for i in range(0,len(personer), 2):
+        for i in range(0,len(personer),2):
                 navn.append(personer[i])
                 alder.append(personer[i + 1])
         print("Navn-liste:", navn)
