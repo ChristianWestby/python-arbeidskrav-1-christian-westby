eac148a9d8e837025863e9ab8beef45ddfba60ad
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-10-01 00:48:09 +0200

    feat: løst oppgave 2.3 (dictionary fra navn/alder) innså feil. Hadde gjort feil oppgave og ferdig på 3.1 (IPv4-sjekk)

--- src/oppgave_2_3.py
+++ src/oppgave_2_3.py
@@ -1,28 +1,13 @@
-def list_splitting():
-    
-        personer = ["Cecilie", 28, "Bjørn", 30,"Tor", 24,"Anna", 25]
+def lag_dictionary():
+    personer = ["Cecilie", 28, "Bjørn", 30, "Tor", 24, "Anna", 25]
 
-        navn = []
-        alder = []
-        for i in range(0,len(personer),2):
-                navn.append(personer[i])
-                alder.append(personer[i + 1])
-        print("Navn-liste:", navn)
-        print("Alder-liste:", alder)
+    dictionary = {}
+    for i in range(0, len(personer), 2):
+        dictionary[personer[i]] = personer[i + 1]
 
-def finn__alder_for_navn():
-    navn_liste = ["Cecilie", "Bjørn", "Tor", "Anna"]
-    alder_liste = [28, 30, 24, 25]
-    navn = input("Skriv inn et navn: ")
-    if navn in navn_liste:
-        index = navn_liste.index(navn)
-        print(f"{navn} er {alder_liste[index]} år gammel.")
-    else:
-        print(f"{navn} finnes ikke i listen.")
-    
-    
+    for navn, alder in dictionary.items():
+        print(f"{navn} er {alder} år")
 
-#Programstart
+# Programstart
 if __name__ == "__main__":
-    list_splitting()
-    finn__alder_for_navn()
+    lag_dictionary()
