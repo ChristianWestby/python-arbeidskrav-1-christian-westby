a86e68df155f6f5247bd5b3fe204efed0aa602e9
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-10-03 12:33:49 +0200

    Gjort ferdig 5.1

---
+++ src/oppgave_5_1.py
@@ -0,0 +1,16 @@
+import csv
+
+def summer_forlengelser(filnavn):
+    total = 0
+    with open(filnavn, "r", encoding="utf-8") as f:
+        leser = csv.DictReader(f, delimiter="\t")  
+        for rad in leser:
+            try:
+                dager = int(rad["Forlenget"].strip())
+                total += dager
+            except:
+                continue
+    print("Total antall forlengede dager:", total)
+
+if __name__ == "__main__":
+    summer_forlengelser("src/bokutlaan.csv")
